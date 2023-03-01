f8404fc41475fc82a650b393721ce586ffbde508
Gage <yaujin@protonmail.com>
2023-02-28 21:56:13 -0600

    made query selector for content div. Created and exported makeHeader function

---
+++ src/home.js
@@ -0,0 +1,35 @@
+const content = document.getElementById('content');
+
+export function makeHeader() {
+  const header = document.createElement('header');
+  content.appendChild(header);
+
+  const nav = document.createElement('nav');
+  header.appendChild(nav);
+
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Roku Sunset';
+  nav.appendChild(h1);
+
+  const homeBtn = document.createElement('button');
+  homeBtn.textContent = 'Home';
+  nav.appendChild(homeBtn);
+
+  const menuBtn = document.createElement('button');
+  menuBtn.textContent = 'Menu';
+  nav.appendChild(menuBtn);
+
+  const contactBtn = document.createElement('button');
+  contactBtn.textContent = 'Contact';
+  nav.appendChild(contactBtn);
+
+  const quoteP = document.createElement('p');
+  quoteP.classList = 'quote';
+  quoteP.textContent =
+    'Only the pure in heart can make a good soup.” <br> -Ludwig Van Beethoven';
+  header.appendChild(quoteP);
+
+  const headerImg = document.createElement('img');
+  headerImg.src = '/src/imgs/ramen-shop.jpg';
+  header.appendChild(headerImg);
+}
