80ea93b813762e9cbe99312d58722dd592977534
Gage <yaujin@protonmail.com>
2023-03-04 18:24:46 -0600

    made makeMainMenu function which made wrapper and all cards with their text content

--- src/menu.js
+++ src/menu.js
@@ -20,3 +20,45 @@ function makeMenuHeader() {
   contactBtn.textContent = 'Contact';
   nav.appendChild(contactBtn);
 }
+
+function makeMainMenu() {
+  const mainMenu = document.createElement('div');
+  mainMenu.classList = 'main-menu';
+  content.appendChild(mainMenu);
+
+  const tonkotsuRamen = document.createElement('div');
+  tonkotsuRamen.classList = 'card';
+  tonkotsuRamen.textContent = 'Tonkotsu Ramen';
+
+  mainMenu.appendChild(tonkotsuRamen);
+
+  const shoyuRamen = document.createElement('div');
+  shoyuRamen.classList = 'card';
+  shoyuRamen.textContent = 'Shoyu Ramen';
+
+  mainMenu.appendChild(shoyuRamen);
+
+  const misoRamen = document.createElement('div');
+  misoRamen.classList = 'card';
+  misoRamen.textContent = 'Miso Ramen';
+
+  mainMenu.appendChild(misoRamen);
+
+  const misoSoup = document.createElement('div');
+  misoSoup.classList = 'card';
+  misoSoup.textContent = 'Miso Soup';
+
+  mainMenu.appendChild(misoSoup);
+
+  const udon = document.createElement('div');
+  udon.classList = 'card';
+  udon.textContent = 'Udon';
+
+  mainMenu.appendChild(udon);
+
+  const soba = document.createElement('div');
+  soba.classList = 'card';
+  soba.textContent = 'Soba';
+
+  mainMenu.appendChild(soba);
+}
