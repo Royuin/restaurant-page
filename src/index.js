46d95f9dea876e52cbb8c9799f1e0d85cfd6658b
Gage <yaujin@protonmail.com>
2023-03-07 20:51:24 -0600

    added page swapping functionality for home and menu

--- src/index.js
+++ src/index.js
@@ -1,2 +1,48 @@
 import { makeMenuPage } from './menu';
 import { makeHomePage } from './home';
+
+makeHomePage();
+
+let homeBtn = document.querySelector('.home-button');
+let menuBtn = document.querySelector('.menu-button');
+
+function addListener() {
+  let homeBtn = document.querySelector('.home-button');
+  let menuBtn = document.querySelector('.menu-button');
+
+  homeBtn.addEventListener('click', () => {
+    const content = document.getElementById('content');
+
+    document.body.removeChild(content);
+
+    makeHomePage();
+    addListener();
+  });
+
+  menuBtn.addEventListener('click', () => {
+    const content = document.getElementById('content');
+
+    document.body.removeChild(content);
+
+    makeMenuPage();
+    addListener();
+  });
+}
+
+homeBtn.addEventListener('click', () => {
+  const content = document.getElementById('content');
+
+  document.body.removeChild(content);
+
+  makeHomePage();
+  addListener();
+});
+
+menuBtn.addEventListener('click', () => {
+  const content = document.getElementById('content');
+
+  document.body.removeChild(content);
+
+  makeMenuPage();
+  addListener();
+});
