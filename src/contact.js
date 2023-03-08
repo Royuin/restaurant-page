e9803ede57aa83d01cc43c9603fd8a01c556dec0
Gage <yaujin@protonmail.com>
2023-03-08 14:16:21 -0600

    made makeMainContent function and added code that makes all the main content

--- src/contact.js
+++ src/contact.js
@@ -26,3 +26,69 @@ function makeContactHeader() {
   contactBtn.classList = 'contact-button';
   nav.appendChild(contactBtn);
 }
+
+function makeMainContent() {
+  const contentMain = document.createElement('main');
+  contentMain.classList = 'contact-main';
+  content.appendChild(contentMain);
+
+  const contactHeading = document.createElement('h2');
+  contactHeading.textContent = 'Contact Us';
+  contentMain.appendChild(contactHeading);
+
+  const locationHeading = document.createElement('h3');
+  locationHeading.textContent = 'Location';
+  contentMain.appendChild(locationHeading);
+  const location = document.createElement('p');
+  location.textContent = '123 Fake Location Blvd. State, Country 12345';
+  contentMain.appendChild(location);
+
+  const hoursHeading = document.createElement('h3');
+  hoursHeading.textContent = 'Hours';
+  contentMain.appendChild(hoursHeading);
+  const hours = document.createElement('p');
+  hours.textContent =
+    'Monday, Tuesday, Wednesday, Thursday: \n 9 am - 10 pm \n Friday, Saturday, Sunday: 10 am - 12 pm';
+  contentMain.appendChild(hours);
+
+  const messageUs = document.createElement('h3');
+  messageUs.textContent = 'Message Us';
+  contentMain.appendChild(messageUs);
+
+  const form = document.createElement('form');
+  contentMain.appendChild(form);
+
+  const nameLabel = document.createElement('label');
+  nameLabel.for = 'name';
+  nameLabel.textContent = 'Name: ';
+  form.appendChild(nameLabel);
+  const nameInput = document.createElement('input');
+  nameInput.type = 'text';
+  nameInput.id = 'name';
+  nameInput.name = 'name';
+  form.appendChild(nameInput);
+
+  const emailLabel = document.createElement('label');
+  emailLabel.for = 'email';
+  emailLabel.textContent = 'Email: ';
+  form.appendChild(emailLabel);
+  const emailInput = document.createElement('input');
+  emailInput.type = 'email';
+  emailInput.name = 'email';
+  emailInput.id = 'email';
+  form.appendChild(emailInput);
+
+  const textAreaLabel = document.createElement('label');
+  textAreaLabel.for = 'message';
+  textAreaLabel.textContent = 'Write your message: ';
+  form.appendChild(textAreaLabel);
+  const textArea = document.createElement('textarea');
+  textArea.id = 'message';
+  textArea.name = 'message';
+  form.appendChild(textArea);
+
+  const submitBtn = document.createElement('button');
+  submitBtn.type = 'submit';
+  submitBtn.textContent = 'Send Message';
+  form.appendChild(submitBtn);
+}
